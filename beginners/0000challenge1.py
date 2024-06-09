ac8c7b7854845cfc12dc97df9324139c0db9c445
Kiryl Bahdanau <kirbah@gmail.com>
2024-06-09 13:57:31 +0200

    0000challenge2

--- beginners/0000challenge1.py
+++ beginners/0000challenge1.py
@@ -1,4 +1,4 @@
-def caesar_cipher(text):
+def vigenere_cipher(text):
   """Shifts characters in text by the given amount."""
   password = "caesar"
   index = 0
@@ -19,11 +19,11 @@ def find_flag(text):
   """Searches for 'FLAG' in the text (case-insensitive)."""
   return "FLAG" in text.upper()
 
-text = """Vhi Nixgnije tkplwr zu a tglpcltzasgtmu sldsxatlvisf czrhij.
+encrypted_text = """Vhi Nixgnije tkplwr zu a tglpcltzasgtmu sldsxatlvisf czrhij.
 Ik ks e eoig sshhzutmuakgd zwrjkor gf kje Gsejcr gapygr, azitj uwws r uirylv uhmxt mclyw tf gngjygv tlw eevivw mvuseye. WNAK{yek_xikyy_nktl_at}"""
 
-shifted_text = caesar_cipher(text)
-if find_flag(shifted_text):
-  print(shifted_text)
+decrypted_text = vigenere_cipher(encrypted_text)
+if find_flag(decrypted_text):
+  print(decrypted_text)
 else:
-  print("FLAG not found after.")
+  print("FLAG not found")
---
+++ beginners/0000challenge2.py
@@ -0,0 +1,30 @@
+def substitute_cipher(text):
+  """Substitute characters to decrypt."""
+  cipher = "PDV KLRBC IOEXQ AEY TLGMF EJVO PDV NSWH ZEU"
+  plaint = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"
+  result = ""
+  for char in text:
+    if char.isalpha():
+      result += plaint[cipher.index(char)]
+    else:
+      result += char
+  return result
+
+def find_flag(text):
+  """Searches for 'FLAG' in the text (case-insensitive)."""
+  return "FLAG" in text.upper()
+
+encrypted_text = """PDV KLRBC IOEXQ AEY TLGMF EJVO PDV NSWH ZEU.
+PDRF PVYP RF S MSQUOSG, XDRBD GVSQF PDSP RP BEQPSRQF SNN 26 NVPPVOF EA PDV VQUNRFD SNMDSIVP. PDRF GSCVF RP RZVSN AEO AOVKLVQBH SQSNHFRF, SF PDV BOHMPSQSNHFP BSQ BEGMSOV PDV AOVKLVQBH EA NVPPVOF RQ PDV BRMDVOPVYP PE PDV CQEXQ AOVKLVQBH EA NVPPVOF RQ PDV VQUNRFD NSQULSUV.
+
+AEO VYSGMNV, PDV GEFP BEGGEQ NVPPVO RQ PDV VQUNRFD NSQULSUV RF V. RA PDV GEFP BEGGEQ NVPPVO RQ PDV BRMDVOPVYP RF Y, PDVQ PDV BOHMPSQSNHFP BSQ SFFLGV PDSP Y RF NRCVNH PE IV S FLIFPRPLPREQ AEO V.
+
+EPDVO BEGGEQ NVPPVOF RQ PDV VQUNRFD NSQULSUV RQBNLZV P, S, E, R, Q, F, SQZ D. PDV BOHMPSQSNHFP BSQ LFV PDRF RQAEOGSPREQ PE GSCV VZLBSPVZ ULVFFVF SIELP PDV EPDVO FLIFPRPLPREQF RQ PDV BRMDVOPVYP.
+
+ANSU{QEX_RJV_NVSOQVZ_GH_SIBF}"""
+
+decrypted_text = substitute_cipher(encrypted_text)
+if find_flag(decrypted_text):
+  print(decrypted_text)
+else:
+  print("FLAG not found after.")
