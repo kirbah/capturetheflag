2dc00875ad7ca33d06202a46c3a48311f3977e7e
Kiryl Bahdanau <kirbah@gmail.com>
2024-06-09 17:11:18 +0200

    1837challenge1

--- beginners/1837challenge1.py
+++ beginners/1837challenge1.py
@@ -53,10 +53,32 @@ def decode_morse(morse_code):
     return result
 
 
-with open('1837challenge0.txt', 'rb') as f:
+def vigenere_cipher(text):
+    """Shifts characters in text by the given amount."""
+    password = "MANCHESTERBLUFFCOMPLETEVICTORY"
+    index = 0
+    result = ""
+    for char in text:
+        if char.isalpha():
+            # Shift only alphabetical characters
+            key_shift = ord(password[index % len(password)]) - ord('A')
+            base = ord('a') if char.islower() else ord('A')
+            new_char = chr((ord(char) - base - key_shift) % 26 + base)
+            result += new_char
+            index += 1
+        else:
+            result += char
+    return result
+
+
+with open('1837challenge1.txt', 'rb') as f:
     text = f.read().decode('utf-8')
-# add spaces around /{} chars
+# add spaces around {} chars
 morse_text = re.sub(r'([/{}])', r'  \1  ', text)
 
 decoded_text = decode_morse(morse_text)
 print(decoded_text)
+crypted = decoded_text[decoded_text.find('{') + 1: decoded_text.find('}')]
+print(f"Crypted: {crypted}")
+decrypt = vigenere_cipher(crypted)
+print(f"Decrypt: {decrypt}")
